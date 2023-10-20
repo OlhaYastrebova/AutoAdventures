fdd7ca752a2a1b1001f441b925a0f60ffa05903c
OlhaYastrebova <116676978+OlhaYastrebova@users.noreply.github.com>
2023-10-20 13:17:04 +0300

    add normalize

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
-// import 'modern-normalize';
 import { Global, ThemeProvider } from '@emotion/react';
 import { GlobalStyles, theme } from './components/styles/index';
 import { App } from './components/App/index';
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
